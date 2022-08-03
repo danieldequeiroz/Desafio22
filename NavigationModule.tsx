6cf1a155a0f847977cb90f3e3f5f3d1d7505204a
danielqueirosene <42477879+danielqueirosene@users.noreply.github.com>
2022-08-02 21:06:38 -0300

    Update NavigationModule.tsx

--- NavigationModule.tsx
+++ NavigationModule.tsx
@@ -1,3 +1,4 @@
+//Código de Daniel de Queiroz Cavalcanti
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
